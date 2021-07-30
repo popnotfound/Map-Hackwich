84daf87515af8e1df12b29880ef961e7af0b94af
popnotfound <aneeshleisure21@gmail.com>
2021-07-30 10:37:54 -0500

    Initial Commit

---
+++ Map Hackwich/Map_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_HackwichApp.swift
+//  Map Hackwich
+//
+//  Created by Aneesh Pushparaj on 7/30/21.
+//
+
+import SwiftUI
+
+@main
+struct Map_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
