c90ed6ac915d87e769deed4421ee7bfe72bd7442
popnotfound <aneeshleisure21@gmail.com>
2021-07-30 10:50:55 -0500

    Added user location

--- Map Hackwich/ContentView.swift
+++ Map Hackwich/ContentView.swift
@@ -15,8 +15,14 @@ struct ContentView: View {
         span: MKCoordinateSpan (
             latitudeDelta: 0.05, longitudeDelta: 0.05)
     )
+    @StateObject var locationManager = LocationManager()
+    @State private var userTrackingMode: MapUserTrackingMode = .follow
     var body: some View {
-        Map(coordinateRegion: $region)
+        Map(coordinateRegion: $region,
+            interactionModes: .all,
+            showsUserLocation: true,
+            userTrackingMode: $userTrackingMode
+        )
     }
 }
 
